773b9a1414dfedde2bcadf76d540c7df84a1d2f8
Diogo Manuel Lourenço Alcântara Guerreiro <diogo.l.guerreiro@tecnico.ulisboa.pt>
2025-05-19 15:34:31 +0100

    Fix: converter logic

--- python/src/converter/convert/to_mp3.py
+++ python/src/converter/convert/to_mp3.py
@@ -1,26 +1,26 @@
 import pika, json, tempfile, os
-import pika.spec
 from bson.objectid import ObjectId
 import moviepy.editor
+
 
 def start(message, fs_videos, fs_mp3s, channel):
     message = json.loads(message)
 
-    #temporary empty file
+    # empty temp file
     tf = tempfile.NamedTemporaryFile()
-    #video content
+    # video contents
     out = fs_videos.get(ObjectId(message["video_fid"]))
-    #add video content to empty file
+    # add video contents to empty file
     tf.write(out.read())
-    #create audio from temp video file
+    # create audio from temp video file
     audio = moviepy.editor.VideoFileClip(tf.name).audio
     tf.close()
 
-    #write the audio to its own file
+    # write audio to the file
     tf_path = tempfile.gettempdir() + f"/{message['video_fid']}.mp3"
     audio.write_audiofile(tf_path)
 
-    #save file to mongo
+    # save file to mongo
     f = open(tf_path, "rb")
     data = f.read()
     fid = fs_mp3s.put(data)
@@ -31,14 +31,13 @@ def start(message, fs_videos, fs_mp3s, channel):
 
     try:
         channel.basic_publish(
-            exchange = "",
-            routing_key = os.environ.get("MP3_QUEUE"),
-            body = json.dumps(message),
-            properties = pika.BasicProperties(
+            exchange="",
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ),
         )
     except Exception as err:
         fs_mp3s.delete(fid)
         return "failed to publish message"
-    
